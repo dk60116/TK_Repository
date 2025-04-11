45156f34871432f96692fc109a8b71556035a7a9
한택근 <dk60116@gmail.com>
2025-04-12 06:07:03 +0900

    Remove all ignored files and keep .gitignore

--- Jusin_FactorioProject/Jusin_FactorioProject.h
+++
@@ -1,3 +0,0 @@
-#include "pch.h"
-#include "resource.h"
-#include "CMainProcess.h"
--- Jusin_FactorioProject/struct.cpp
+++
@@ -1,6 +0,0 @@
-#include "struct.h"
-
-const vector2Int vector2::toVector2Int() const
-{
-	return vector2Int((int)round(x), (int)round(y));
-}
