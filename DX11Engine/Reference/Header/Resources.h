4593ffa1a70edd50d1e9fd1bc2fada6b13cf361a
HTK <dk60116@gmail.com>
2025-08-06 04:17:13 +0900

    Feat: Navimesh File Out

--- DX11Engine/Reference/Header/Resources.h
+++ DX11Engine/Reference/Header/Resources.h
@@ -44,7 +44,7 @@ public:
     vector<CMeshBuffer::MeshBufferInitiaizeInfo> ReadMeshBufferInfos(const wstring _binFileName);
     HRESULT SaveSkinnedBufferInfos(const wstring _filePath, vector<CSkinnedMeshBuffer::SkinnedBufferInitiaizeInfo> _infoList, vector<CSkinnedMeshBuffer::SKINNEDSKELETAL> _skeletonInfo);
     CSkinnedMeshBuffer::SkinnedBuffer ReadSkinnedBufferInfos(const wstring _binFileName);
-    HRESULT SaveNaviMeshBufferInfos(const wstring _filePath, EngineAI::CNaviMesh::NaviMeshBufferInitiaizeInfo _infoList);
+    HRESULT SaveNaviMeshBufferInfos(const wstring _filePath, EngineAI::CNaviMesh::NaviMeshBufferInitiaizeInfo _info);
     EngineAI::CNaviMesh::NaviMeshBufferInitiaizeInfo ReadNaviBufferInfos(const wstring _binFileName);
     HRESULT SaveAnimationClipBufferInfos(const wstring _filePath, vector<CAnimationClip::AnimationClipInitInfo> _infoList);
     vector<CAnimationClip::AnimationClipInitInfo> ReadAnimationClipBufferInfos(const wstring _binFileName);
