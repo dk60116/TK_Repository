2f9c6033b1591fa23b4777944c6f2476282ac29d
HTK <dk60116@gmail.com>
2025-09-22 02:26:23 +0900

    Feat:: Particle Shader

--- DX11Engine/Client/EngineResources/Shader/Particle.hlsl
+++ DX11Engine/Client/EngineResources/Shader/Particle.hlsl
@@ -152,91 +152,10 @@ VSOut VSMain(VSIn v)
 // «»ºø ºŒ¿Ã¥ı
 float4 PSMain(VSOut input) : SV_TARGET
 {
-    float2 tillingUV = float2(input.uv.x * gTiling.x + gOffset.x, input.uv.y * gTiling.y + gOffset.y);
-    float4 texColor = useTexture ? gBasemap.Sample(gSampler, tillingUV) * baseColor : baseColor;
+    float4 texColor = useTexture ? gBasemap.Sample(gSampler, input.uv) * baseColor : baseColor;
     
     if (texColor.a < 0.01f)
         discard;
 
-    float3 N = normalize(input.normalW);
-    float3 T = normalize(input.tangentW);
-    float3 B = normalize(cross(N, T));
-    float3 V = normalize(pos - input.posW);
-    
-    float3x3 TBN = float3x3(T, B, N);
-    
-    float4 nm = gNormalmap.Sample(gSampler, tillingUV);
-
-    
-    if (any(nm))
-    {
-        float3 normalMap = nm.rgb;
-        normalMap = normalize(normalMap * 2.0f - 1.0f);
-        N = normalize(mul(normalMap, TBN));
-    }
-    
-    if (gCalcLight == 0)
-        return texColor;
-  
-    float3 diffuseSum = float3(0, 0, 0);
-    float3 ambientSum = float3(0, 0, 0);
-    float3 specularSum = float3(0, 0, 0);
-
-    int lightCount = (int) gLight[0][3][3];
-
-    for (int i = 0; i < lightCount; ++i)
-    {
-        if (gLight[i][3][2] < 0.5)
-            continue;
-
-        uint lightType = (uint) gLight[i][3][0];
-        float3 lightPos = float3(gLight[i][0][0], gLight[i][0][1], gLight[i][0][2]);
-        float3 lightDir = float3(gLight[i][1][0], gLight[i][1][1], gLight[i][1][2]);
-        float3 lightColor = float3(gLight[i][2][0], gLight[i][2][1], gLight[i][2][2]);
-        float intensity = gLight[i][1][3];
-        float range = gLight[i][0][3];
-        float attenuationK = gLight[i][3][1];
-        float ambientK = gLight[i][2][3];
-
-        float3 L;
-        float attenuation = 1.0f;
-
-        if (lightType == LIGHT_TYPE_DIRECTIONAL)
-        {
-            L = normalize(-lightDir);
-        }
-        else if (lightType == LIGHT_TYPE_POINT)
-        {
-            float3 toLight = lightPos - input.posW;
-            float dist = length(toLight);
-            L = toLight / dist;
-            attenuation = saturate(1.0f - dist / range) * attenuationK;
-        }
-        else
-            continue;
-
-        // Diffuse
-        float NdotL = saturate(dot(N, L));
-        float3 diffuse = lightColor * NdotL * intensity * attenuation;
-        diffuseSum += diffuse;
-        
-        // Specular
-        float3 R = reflect(-L, N);
-        float RdotV = saturate(dot(R, V));
-        float3 fSpecular = pow(RdotV, 50);
-        float specularStrength = gSmoothness;
-        float3 specular = lightColor * fSpecular * specularStrength * intensity * attenuation;
-        specularSum += specular;
-        
-        // Ambient
-        float3 ambient = lightColor * ambientK * attenuation;
-        ambientSum += ambient;
-    }
-
-    float3 litDiffuse = texColor.rgb * saturate(ambientSum + diffuseSum);
-    float3 finalColor = litDiffuse + specularSum;
-  
-    finalColor = saturate(finalColor);
-
-    return float4(finalColor, texColor.a);
+    return float4(texColor.rgb, texColor.a);
 }
